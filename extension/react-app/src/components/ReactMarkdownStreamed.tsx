3b43a012a1cdb7e5021676605200a62b6493ed2b
Nate Sesti <sestinj@gmail.com>
2023-07-13 14:20:47 -0700

    Turns out replacing deletes selection : (

--- extension/react-app/src/components/ReactMarkdownStreamed.tsx
+++ extension/react-app/src/components/ReactMarkdownStreamed.tsx
@@ -14,170 +14,196 @@ function compareAndUpdateDomNode(old: Node, new_: Node) {
   console.log(old, new_);
   if (typeof old === "undefined" || typeof new_ === "undefined") return;
 
-  // If the node types are completely different, replace the old node with the new node
-  if (old.parentNode && old.nodeName !== new_.nodeName) {
-    old.parentNode!.replaceChild(new_, old);
-    return;
-  } else if (old.nodeName !== new_.nodeName) {
-    throw Error("old and new node names are different, but old has no parent");
-  }
-
-  // // Second base case: If all child nodes are text nodes
-  // let allChildrenAreTextNodes = true;
-  // for (let i = 0; i < old.childNodes.length; i++) {
-  //   if (old.childNodes[i].nodeType !== Node.TEXT_NODE) {
-  //     allChildrenAreTextNodes = false;
-  //     break;
-  //   }
-  // }
-
-  // if (allChildrenAreTextNodes) {
-  //   if (old.textContent !== new_.textContent) {
-  //     const deltaContent = new_.textContent!.slice(
-  //       old.textContent!.length,
-  //       new_.textContent!.length
-  //     );
-  //     const deltaNode = document.createTextNode(deltaContent);
-  //     console.log(
-  //       "2 delta: ",
-  //       deltaContent,
-  //       "|",
-  //       old.textContent,
-  //       "|",
-  //       new_.textContent
-  //     );
-  //     old.appendChild(deltaNode);
-  //   }
-  //   return;
-  // }
-  // // Base case: if the old node is a text node, update it
-  // if (old.nodeType === Node.TEXT_NODE) {
-  //   if (old.textContent !== new_.textContent) {
-  //     const deltaContent = new_.textContent!.slice(
-  //       old.textContent!.length,
-  //       new_.textContent!.length
-  //     );
-  //     const deltaNode = document.createTextNode(deltaContent);
-  //     console.log("1 delta: ", deltaContent);
-  //     old.parentNode!.appendChild(deltaNode);
-  //   }
-  //   return;
-  // }
-
-  // let i_old = 0;
-  // let i_new = 0;
-  // let oldTextContentBuffer = "";
-  // let newTextContentBuffer = "";
-  // let oldStillText = true;
-  // while (i_old < old.childNodes.length || i_new < new_.childNodes.length) {
-  //   if (old.childNodes[i_old].nodeType === Node.TEXT_NODE) {
-  //     oldTextContentBuffer += old.childNodes[i_old].textContent!;
-  //     oldStillText = true;
-  //   } else {
-  //     oldStillText = false;
-  //   }
-
-  //   if (oldStillText) {
-  //   } else {
-  //     if (new_.childNodes[i_new].nodeType === Node.TEXT_NODE) {
-  //       newTextContentBuffer += new_.childNodes[i_new].textContent!;
-  //       i_new++;
-  //     } else {
-  //     }
-  //   }
-
-  //   i_old++;
-  // }
-
-  let oldTextBuffer = "";
-  let offset = 0;
-  let i = 0;
-
-  for (i; i < old.childNodes.length; i++) {
-    if (old.childNodes[i].nodeType === Node.TEXT_NODE) {
-      oldTextBuffer += old.childNodes[i].textContent!;
-      offset++;
-    } else if (oldTextBuffer.length > 0) {
-      const newTextNode = new_.childNodes[i - offset];
-      if (newTextNode.nodeType !== Node.TEXT_NODE)
-        throw Error("new should be text node");
-      const deltaContent = new_.textContent!.slice(
-        oldTextBuffer.length,
-        newTextNode.textContent!.length
-      );
-      const deltaNode = document.createTextNode(deltaContent);
-
-      old.replaceChild(deltaNode, old.childNodes[i]);
-      // old.appendChild(deltaNode);
-
-      oldTextBuffer = "";
-    } else {
-      compareAndUpdateDomNode(old.childNodes[i], new_.childNodes[i - offset]);
-      oldTextBuffer = "";
-    }
-  }
-
-  if (oldTextBuffer.length > 0) {
-    const newTextNode = new_.childNodes[i - offset];
-    // if (newTextNode.nodeType !== Node.TEXT_NODE)
-    //   throw Error("new should be text node");
-    const deltaContent = new_.textContent!.slice(
-      oldTextBuffer.length,
-      newTextNode.textContent!.length
-    );
-    console.log("Adding text at", i - offset, deltaContent);
-    const deltaNode = document.createTextNode(deltaContent);
-    old.appendChild(deltaNode);
-    oldTextBuffer = "";
-    i++;
-  }
-
-  if (i - offset < new_.childNodes.length) {
-    for (let j = i - offset; j < new_.childNodes.length; j++) {
-      console.log("Appending new node: ", new_.childNodes[j]);
-      // Backtrack to replace old text nodes that have become this other node
-
-      // Initialize an empty string to accumulate the text content of the old nodes
-      let accumulatedText = "";
-      let newNodeText = new_.childNodes[j].textContent!;
-
-      // Iterate over the child nodes of the old node in reverse order
-      for (let i = old.childNodes.length - 1; i >= 0; i--) {
-        let oldNode = old.childNodes[i];
-
-        // Only consider text nodes
-        if (oldNode.nodeType === Node.TEXT_NODE) {
-          // Prepend the text content of the old node to the accumulated text
-          accumulatedText = oldNode.textContent + accumulatedText;
-
-          // If the accumulated text starts with the text content of the new node
-          if (accumulatedText.endsWith(newNodeText)) {
-            // Remove the old nodes that were included in the accumulated text
-            for (let j = old.childNodes.length - 1; j >= i; j--) {
-              old.removeChild(old.childNodes[j]);
-            }
-
-            break;
-          }
-        } else {
-          break;
-        }
+  // Base case: Text nodes are always replaced
+  if (old.nodeType === Node.TEXT_NODE) {
+    if (new_.nodeType === Node.TEXT_NODE) {
+      if (old.textContent !== new_.textContent) {
+        const deltaContent = new_.textContent!.slice(
+          old.textContent!.length,
+          new_.textContent!.length
+        );
+        const deltaNode = document.createTextNode(deltaContent);
+        old.parentNode!.appendChild(deltaNode);
       }
-
-      old.appendChild(new_.childNodes[j]);
+    } else {
+      old.parentNode!.replaceChild(new_, old);
     }
+    return;
   }
 
-  // const numOldChildNodes = old.hasChildNodes() ? old.childNodes.length : 0;
-  // console.log(numOldChildNodes);
-  // for (let i = 0; i < numOldChildNodes; i++) {
-  //   compareAndUpdateDomNode(old.childNodes[i], new_.childNodes[i]);
-  // }
-  // for (let i = numOldChildNodes; i < new_.childNodes.length; i++) {
-  //   console.log("Appending new node: ", new_.childNodes[i]);
-  //   old.appendChild(new_.childNodes[i]);
-  // }
+  const numOldChildNodes = old.hasChildNodes() ? old.childNodes.length : 0;
+  for (let i = 0; i < numOldChildNodes; i++) {
+    compareAndUpdateDomNode(old.childNodes[i], new_.childNodes[i]);
+  }
+  for (let i = numOldChildNodes; i < new_.childNodes.length; i++) {
+    console.log("Appending new node: ", new_.childNodes[i]);
+    old.appendChild(new_.childNodes[i]);
+  }
 }
+//   // If the node types are completely different, replace the old node with the new node
+//   if (old.parentNode && old.nodeName !== new_.nodeName) {
+//     old.parentNode!.replaceChild(new_, old);
+//     return;
+//   } else if (old.nodeName !== new_.nodeName) {
+//     throw Error("old and new node names are different, but old has no parent");
+//   }
+
+//   // // Second base case: If all child nodes are text nodes
+//   // let allChildrenAreTextNodes = true;
+//   // for (let i = 0; i < old.childNodes.length; i++) {
+//   //   if (old.childNodes[i].nodeType !== Node.TEXT_NODE) {
+//   //     allChildrenAreTextNodes = false;
+//   //     break;
+//   //   }
+//   // }
+
+//   // if (allChildrenAreTextNodes) {
+//   //   if (old.textContent !== new_.textContent) {
+//   //     const deltaContent = new_.textContent!.slice(
+//   //       old.textContent!.length,
+//   //       new_.textContent!.length
+//   //     );
+//   //     const deltaNode = document.createTextNode(deltaContent);
+//   //     console.log(
+//   //       "2 delta: ",
+//   //       deltaContent,
+//   //       "|",
+//   //       old.textContent,
+//   //       "|",
+//   //       new_.textContent
+//   //     );
+//   //     old.appendChild(deltaNode);
+//   //   }
+//   //   return;
+//   // }
+//   // // Base case: if the old node is a text node, update it
+//   // if (old.nodeType === Node.TEXT_NODE) {
+//   //   if (old.textContent !== new_.textContent) {
+//   //     const deltaContent = new_.textContent!.slice(
+//   //       old.textContent!.length,
+//   //       new_.textContent!.length
+//   //     );
+//   //     const deltaNode = document.createTextNode(deltaContent);
+//   //     console.log("1 delta: ", deltaContent);
+//   //     old.parentNode!.appendChild(deltaNode);
+//   //   }
+//   //   return;
+//   // }
+
+//   // let i_old = 0;
+//   // let i_new = 0;
+//   // let oldTextContentBuffer = "";
+//   // let newTextContentBuffer = "";
+//   // let oldStillText = true;
+//   // while (i_old < old.childNodes.length || i_new < new_.childNodes.length) {
+//   //   if (old.childNodes[i_old].nodeType === Node.TEXT_NODE) {
+//   //     oldTextContentBuffer += old.childNodes[i_old].textContent!;
+//   //     oldStillText = true;
+//   //   } else {
+//   //     oldStillText = false;
+//   //   }
+
+//   //   if (oldStillText) {
+//   //   } else {
+//   //     if (new_.childNodes[i_new].nodeType === Node.TEXT_NODE) {
+//   //       newTextContentBuffer += new_.childNodes[i_new].textContent!;
+//   //       i_new++;
+//   //     } else {
+//   //     }
+//   //   }
+
+//   //   i_old++;
+//   // }
+
+//   let oldTextBuffer = "";
+//   let offset = 0;
+//   let i = 0;
+
+//   for (i; i < old.childNodes.length; i++) {
+//     if (old.childNodes[i].nodeType === Node.TEXT_NODE) {
+//       oldTextBuffer += old.childNodes[i].textContent!;
+//       offset++;
+//     } else if (oldTextBuffer.length > 0) {
+//       const newTextNode = new_.childNodes[i - offset];
+//       if (newTextNode.nodeType !== Node.TEXT_NODE)
+//         throw Error("new should be text node");
+//       const deltaContent = new_.textContent!.slice(
+//         oldTextBuffer.length,
+//         newTextNode.textContent!.length
+//       );
+//       const deltaNode = document.createTextNode(deltaContent);
+
+//       old.replaceChild(deltaNode, old.childNodes[i]);
+//       // old.appendChild(deltaNode);
+
+//       oldTextBuffer = "";
+//     } else {
+//       compareAndUpdateDomNode(old.childNodes[i], new_.childNodes[i - offset]);
+//       oldTextBuffer = "";
+//     }
+//   }
+
+//   if (oldTextBuffer.length > 0) {
+//     const newTextNode = new_.childNodes[i - offset];
+//     // if (newTextNode.nodeType !== Node.TEXT_NODE)
+//     //   throw Error("new should be text node");
+//     const deltaContent = new_.textContent!.slice(
+//       oldTextBuffer.length,
+//       newTextNode.textContent!.length
+//     );
+//     console.log("Adding text at", i - offset, deltaContent);
+//     const deltaNode = document.createTextNode(deltaContent);
+//     old.appendChild(deltaNode);
+//     oldTextBuffer = "";
+//     i++;
+//   }
+
+//   if (i - offset < new_.childNodes.length) {
+//     for (let j = i - offset; j < new_.childNodes.length; j++) {
+//       console.log("Appending new node: ", new_.childNodes[j]);
+//       // Backtrack to replace old text nodes that have become this other node
+
+//       // Initialize an empty string to accumulate the text content of the old nodes
+//       let accumulatedText = "";
+//       let newNodeText = new_.childNodes[j].textContent!;
+
+//       // Iterate over the child nodes of the old node in reverse order
+//       for (let i = old.childNodes.length - 1; i >= 0; i--) {
+//         let oldNode = old.childNodes[i];
+
+//         // Only consider text nodes
+//         if (oldNode.nodeType === Node.TEXT_NODE) {
+//           // Prepend the text content of the old node to the accumulated text
+//           accumulatedText = oldNode.textContent + accumulatedText;
+
+//           // If the accumulated text starts with the text content of the new node
+//           if (accumulatedText.endsWith(newNodeText)) {
+//             // Remove the old nodes that were included in the accumulated text
+//             for (let j = old.childNodes.length - 1; j >= i; j--) {
+//               old.removeChild(old.childNodes[j]);
+//             }
+
+//             break;
+//           }
+//         } else {
+//           break;
+//         }
+//       }
+
+//       old.appendChild(new_.childNodes[j]);
+//     }
+//   }
+
+//   // const numOldChildNodes = old.hasChildNodes() ? old.childNodes.length : 0;
+//   // console.log(numOldChildNodes);
+//   // for (let i = 0; i < numOldChildNodes; i++) {
+//   //   compareAndUpdateDomNode(old.childNodes[i], new_.childNodes[i]);
+//   // }
+//   // for (let i = numOldChildNodes; i < new_.childNodes.length; i++) {
+//   //   console.log("Appending new node: ", new_.childNodes[i]);
+//   //   old.appendChild(new_.childNodes[i]);
+//   // }
+// }
 
 function ReactMarkdownStreamed(props: { source: string }) {
   const rootRef = React.useRef<HTMLDivElement>(null);
