1b6866b77853e259147a984834c62239c836b466
Nate Sesti <sestinj@gmail.com>
2023-07-13 12:52:07 -0700

    checkpoint, somewhat working, except need to replace, not add a ton of new text nodes

--- extension/react-app/types/markdown.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "markdown" {
-  const markdown = {
-    toHTML: function (markdown: string): string {
-      // implementation here
-    },
-  };
-  export { markdown };
-}
---
+++ extension/react-app/types/marked.d.ts
@@ -0,0 +1,8 @@
+declare module "marked" {
+  const marked = {
+    parse: function (markdown: string): string {
+      // implementation here
+    },
+  };
+  export { marked };
+}
